ac4fcedbdacc69623490c9657fb6eb89a09ce8da
BluKnight98 <70171431+BluKnight98@users.noreply.github.com>
2021-02-19 01:32:24 -0700

    fetchSmurfs created

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,6 +3,19 @@ export const FETCH_START = 'FETCH_START';
 export const FETCH_SUCCESS = 'FETCH_SUCCESS';
 export const FETCH_ERROR = 'FETCH_ERROR';
 
+export const fetchSmurfs = () => {
+    return dispatch => {
+        dispatch({ type: FETCH_START });
+        axios.get('http://localhost:3333/smurfs')
+            .then((res) => {
+                console.log(res);
+            })
+            .catch((err) => {
+                console.log(err);
+            })
+    }
+}
+
 //Task List:
 //1. Add a thunk action called fetchSmurfs that triggers a loading status display in our application, performs an axios call to retreive smurfs from our server, saves the result of that call to our state and shows an error if one is made.
 //2. Add a standard action that allows us to add new smurf (including the name, nickname, position, summary)
